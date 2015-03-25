d16558d49d1f8adf796499f2843efd84cfbca9f9
jeje2208 <baziinje@gmail.com>
2015-03-25 22:49:19 +0100

    mise a jour de AFaire

---
+++ zzDev-jeje2208/src/jeje2208/AFaire.java
@@ -0,0 +1,11 @@
+package jeje2208;
+
+public class AFaire {
+	
+	/*
+	 * 
+	 * commLobby mettre les reqette sql dans RecupConfigListServerName
+	 * mettre en place les bases de donnnee
+	 * 
+	 */
+}
