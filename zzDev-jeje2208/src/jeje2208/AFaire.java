45a2c5b5821bc34a1b9c27eac1d7894ebdf9c85b
jeje2208 <baziinje@gmail.com>
2015-03-25 22:50:31 +0100

    AFaire

--- zzDev-jeje2208/src/jeje2208/AFaire.java
+++ zzDev-jeje2208/src/jeje2208/AFaire.java
@@ -2,10 +2,20 @@ package jeje2208;
 
 public class AFaire {
 	
+	//Partie commencé
+	
 	/*
 	 * 
 	 * commLobby mettre les reqette sql dans RecupConfigListServerName
 	 * mettre en place les bases de donnnee
+	 * 
+	 */
+	
+	//Partie pas commencé
+	
+	/*
+	 * 
+	 * 
 	 * 
 	 */
 }
