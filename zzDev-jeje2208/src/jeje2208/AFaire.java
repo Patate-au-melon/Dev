e48c3971137a46ff4d44fc25b122c684b8d98690
jeje2208 <baziinje@gmail.com>
2015-04-02 19:21:07 +0200

    AFAire

--- zzDev-jeje2208/src/jeje2208/AFaire.java
+++ zzDev-jeje2208/src/jeje2208/AFaire.java
@@ -8,7 +8,6 @@ public class AFaire {
 	 * 
 	 * tenter de resoudre le probleme lie a la deconnexion d'un serveur /pb buffered etc... non ferme
 	 * probleme lors de l'echange de message entre les serveurs --> lobby, erreur sur la ligne 50 de server.ReceiveMessage
-	 * base de donnee pour sauvegarder la liste et la position des panneau 
 	 *
 	 */
 	
@@ -16,7 +15,6 @@ public class AFaire {
 	
 	/*
 	 * 
-	 * regarder si la couleur passe sur les panneaux
 	 * mettre en place le systeme de config avance
 	 * tester si il y a un probleme lors de l'echange de message dans le sens lobby --> serveurs
 	 * base de donnee pour les moneys, plus une autre pour les stats
@@ -28,9 +26,6 @@ public class AFaire {
 	 /*
 	 *
 	 * voir avec le dev web pour utilisation des socket en php
-	 * gestion base de donnee pour obtenir la liste de tous les panneaux en cas de non connexion des serveurs
-	 * voir avec salunaire pour le systeme de couleur et creation d'un system random
-	 * voir avec salunaire pour les armures a couleur fixe pour le pannel de selection
 	 *
 	 */
 	 
